b9a31afaa3286de8bff57fd7b926feb9a223496b
Constantin Beer <constantin.beer@outlook.de>
2024-10-28 14:06:41 +0100

    initial commit

---
+++ src/test/java/ch/baloise/pactdemo/controller/GreetingControllerPactTest.java
@@ -0,0 +1,36 @@
+package ch.baloise.pactdemo.controller;
+
+import au.com.dius.pact.provider.PactVerifyProvider;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Provider("provider")
+@PactFolder("pacts")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class GreetingControllerPactTest {
+
+    @BeforeEach
+    void setup(PactVerificationContext context) {
+        context.setTarget(new HttpTestTarget("localhost", 8080));
+    }
+
+    @State("default")
+    public void toDefaultState() {
+        // Set up the default state required for the Pact test
+        // If no setup is required, you can leave this method empty
+    }
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+}
